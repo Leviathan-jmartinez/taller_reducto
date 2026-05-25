7ef97705f0c5be7d13b0baeb62e82ce95f2889ca
JuanM <juanmartinez076@gmail.com>
2026-05-24 23:49:35 -0300

    cambios en loginajax quitar echo

--- ajax/loginAjax.php
+++ ajax/loginAjax.php
@@ -5,11 +5,11 @@ require_once "../config/APP.php";
 if (isset($_POST['usuario'])) {
     require_once "../controladores/loginControlador.php";
     $inst_login = new loginControlador();
-    echo $inst_login->cierre_sesion_controlador();
+    $inst_login->cierre_sesion_controlador();
 } elseif (isset($_POST['accion']) && $_POST['accion'] === 'cambiar_clave_obligatoria') {
     require_once "../controladores/loginControlador.php";
     $inst_login = new loginControlador();
-    echo $inst_login->cambiar_clave_obligatoria_controlador();
+    $inst_login->cambiar_clave_obligatoria_controlador();
 } else {
     session_start(['name' => 'STR']);
     session_unset();
